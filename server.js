a9788ab9f77cdd7db9f114266e98941ccfd43d4b
Smitha Renny <30974556+SmithaRenny@users.noreply.github.com>
2017-08-26 16:26:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-    re.send("Article One has been requested and will be served here");
+    res.send("Article One has been requested and will be served here");
 });
 
-app.get('/article-two',function(){
-    re.send("Article Two has been requested and will be served here");
+app.get('/article-two',function(req, res){
+    res.send("Article Two has been requested and will be served here");
 });
 
-app.get('/article-three',function(){
-    re.send("Article Three has been requested and will be served here");
+app.get('/article-three',function(req, res){
+    res.send("Article Three has been requested and will be served here");
 });
 
 app.get('/ui/style.css', function (req, res) {
