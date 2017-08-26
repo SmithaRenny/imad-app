dbb32c796909db203af9eca85417f6f717661311
Smitha Renny <30974556+SmithaRenny@users.noreply.github.com>
2017-08-26 16:25:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(){
+app.get('/article-one',function(req, res){
     re.send("Article One has been requested and will be served here");
 });
 
